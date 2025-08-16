5da6aa926e86cdfafe7aa188d86e7679a68b5614
Zico <zico@Mac.lan>
2025-08-16 20:55:05 +0200

    Additional analysis added with first chart

--- src/analyze_expenses.py
+++ src/analyze_expenses.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os #helps me navigate with my folders as had issues locating folders for the files and print(os.getcwd)) does magic!
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('../data/sample_data.csv') #reading .csv file to pandas
 
@@ -20,3 +21,33 @@ expenses = df[df['Kwota'] < 0]['Kwota'].sum()
 print(f'Sum of incomes: {incomes:.2f}')
 print(f'Sum of expenses: {expenses:.2f}')
 print(f'Balance: {incomes + expenses:.2f}')
+
+#Let's add 10 biggest spending from the file
+top_expenses = df[df['Kwota'] < 0].sort_values(by='Kwota').head(10).reset_index(drop = True)
+print('Top 10 expenses:')
+print(top_expenses[['Data operacji', 'Kwota', 'Opis transakcji']])
+
+#Quick check to convert 'Data Operacji' to date (if not it will be change to NaT)
+df['Data operacji'] = pd.to_datetime(df['Data operacji'], errors='coerce')
+
+#Creates a table that shows how much was spent on particular date
+daily_expenses = df[df['Kwota'] < 0].groupby(df['Data operacji'].dt.date)['Kwota'].sum()
+daily_incomes = df[df['Kwota'] > 0].groupby(df['Data operacji'].dt.date)['Kwota'].sum()
+daily_balance = df.groupby(df["Data operacji"].dt.date)["Kwota"].sum()
+
+print('Daily expenses report: \n')
+print(daily_expenses.head(10))
+print('Daily incomes report: \n')
+print(daily_incomes.head(10))
+print('Daily balance report: \n')
+print(daily_balance.head(10))
+
+#Adding a chart showing sum od daily transactions
+print('Sum of expenses and incomes on particlar days: ')
+plt.figure(figsize=(10,5))
+daily_balance.plot(kind="bar")
+plt.title("Bilans dzienny")
+plt.xlabel("Data")
+plt.ylabel("Kwota [PLN]")
+plt.xticks(rotation=45)
+plt.show()
